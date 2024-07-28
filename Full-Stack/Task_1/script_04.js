b8f0de119c2e6b536082dc92d7e792f90df87cb0
mdaashir <s.mohamedaashir@gmail.com>
2024-07-28 07:00:39 +0530

    style: Fix capitalization in CSS class names and update button styles

--- Full-Stack/Task_1/script_04.js
+++ Full-Stack/Task_1/script_04.js
@@ -1,5 +1,5 @@
 function showPopup() {
-    alert("To view the result, please open the browser console window.");
+	alert('To view the result, please open the browser console window.');
 }
 var num1 = 10;
 var num2 = 20;
@@ -11,20 +11,20 @@ var num5 = 50;
 var numbers = [num1, num2, num3, num4, num5];
 
 for (var i = 0; i < numbers.length; i++) {
-    console.log("Value of num" + (i + 1) + ":", numbers[i]);
+	console.log('Value of num' + (i + 1) + ':', numbers[i]);
 }
 
 // Add the numbers
 var sum = num1 + num2 + num3 + num4 + num5;
-console.log("Sum of the numbers:", sum);
+console.log('Sum of the numbers:', sum);
 
 // Subtract 40 from the sum
 var result = sum - 40;
-console.log("Result of the subtraction:", result);
+console.log('Result of the subtraction:', result);
 
 // Multiply the result by 30
 var finalResult = result * 30;
-console.log("Result of the multiplication:", finalResult)
+console.log('Result of the multiplication:', finalResult);
 
 // Output the final result
-console.log("Final Result:", finalResult);
+console.log('Final Result:', finalResult);

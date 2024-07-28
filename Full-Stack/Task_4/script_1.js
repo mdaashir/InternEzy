0af34558d85e1b3842863a9be1640e19ecc1e320
mdaashir <s.mohamedaashir@gmail.com>
2024-07-28 15:32:45 +0530

    Refactor weather app layout and styles

--- Full-Stack/Task_4/script_1.js
+++ Full-Stack/Task_4/script_1.js
@@ -1,1 +1,19 @@
+// @ts-nocheck
 /* External JS */
+let cityName = document.getElementById('city');
+
+cityName.addEventListener('input', function () {
+	let city = cityName.value;
+	// getWeather(city);
+});
+
+function getWeatherIcon(iconCode) {
+	const iconBaseUrl = 'https://openweathermap.org/img/wn/';
+	const iconSize = '@2x.png';
+	return `<img src="${iconBaseUrl}${iconCode}${iconSize}" alt="Weather Icon">`;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	// getWeather();
+	setInterval(getWeather, 900000);
+});

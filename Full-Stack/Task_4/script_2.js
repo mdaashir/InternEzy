f7a948ecd3b71dc0050021a7a65e870ed3125295
mdaashir <s.mohamedaashir@gmail.com>
2024-07-30 04:25:22 +0530

    feat: Add async function to fetch product data from API

--- Full-Stack/Task_4/script_2.js
+++ Full-Stack/Task_4/script_2.js
@@ -1,1 +1,14 @@
 /* External JS */
+
+async function getProduct() {
+	try {
+		// const response = await fetch('https://fakestoreapi.com/products');
+		const response = await fetch('https://dummyjson.com/products');
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+getProduct();

d1d280d409d5ac30e6409b0feeaddc7791dfe20e
mdaashir <s.mohamedaashir@gmail.com>
2024-07-30 19:08:42 +0530

    chore: Refactor showTodos function to improve code readability and maintainability

--- Full-Stack/Task_4/script_3.js
+++ Full-Stack/Task_4/script_3.js
@@ -7,3 +7,13 @@ let todosJson = JSON.parse(localStorage.getItem('todos') ?? '') || [];
 const deleteAllButton = document.querySelector('.delete-all');
 const filters = document.querySelectorAll('.filter');
 let filter = '';
+
+function showTodos() {
+	if (todosJson.length == 0) {
+		todosHtml.innerHTML = '';
+		emptyImage.style.display = 'block';
+	} else {
+		todosHtml.innerHTML = todosJson.map(getTodoHtml).join('');
+		emptyImage.style.display = 'none';
+	}
+}
